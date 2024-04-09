707d2c126ad8b216e3c6cb50608cf1d188e1ecf8
RyanCPotts <ryancpotts@gmail.com>
2024-04-08 19:20:03 -0500

    book componenets

--- src/About.jsx
+++ src/About.jsx
@@ -1,11 +1,18 @@
 import { Component } from "react";
-
 class Profile extends Component {
-
   render() {
     /* TODO: render information about the developers */
-    return <p>Profile page coming soon</p>
+    return(
+      <>
+        <h1>AboutUs</h1>
+        <h2>Ryan Potts</h2>
+        <p>Hi, I'm Ryan Potts</p>
+        <a href="https://github.com/RyanCPotts">My Github</a>
+        <h2>Qilin Xie</h2>
+        <p>Hi, I'm Qilin Xie</p>
+        <a href="https://github.com/QILINXIE02">My Github</a>
+      </>
+    )
   }
 }
-
 export default Profile;
